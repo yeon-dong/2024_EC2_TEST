f37824a956b4688e74966758132cd2c1afa18a4e
yeon-dong <jae073@naver.com>
2024-10-11 17:37:12 +0900

    commit

--- Ex11_Food/src/main/java/com/sample/spring/repository/FoodRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sample.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sample.spring.model.FoodEntity;
-
-public interface FoodRepository extends JpaRepository<FoodEntity, Long> {
-
-}
